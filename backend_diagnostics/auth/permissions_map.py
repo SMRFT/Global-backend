a34fdc259df40021a544824582b31658ef274c29
SMRFT <parthibansmrft@gmail.com>
2025-04-17 16:06:36 +0530

    global permissions_map.py changes

--- backend_diagnostics/auth/permissions_map.py
+++ backend_diagnostics/auth/permissions_map.py
@@ -1,13 +1,19 @@
 PAGE_MAPPING = {
-    '/admin_registration/':'GL-P-EAD',
+    '/_b_a_c_k_e_n_d/Global/adminreg/':'GL-P-EAD',
+    '/_b_a_c_k_e_n_d/Global/create_employee/':'GL-P-EP', 
+    '/_b_a_c_k_e_n_d/Global/set_employee_password/':'GL-P-EL',
+    '/_b_a_c_k_e_n_d/Global/update_department/<str:department_code>/':'GL-P-EAD',
+    '/_b_a_c_k_e_n_d/Global/update_designation/<str:designation_code>/':'GL-P-EAD',
+
+    '/adminreg/':'GL-P-EAD',
     '/create_employee/': 'GL-P-EP',
     '/set_employee_password/':'GL-P-EL',
-    '/update_department/':'GL-P-EAD',
-    '/update_designation/':'GL-P-EAD',
+    '/update_department/<str:department_code>/':'GL-P-EAD',
+    '/update_designation/<str:designation_code>/':'GL-P-EAD',    
 }
 
 PAGE_ACTION_MAPPING = {
-    'GL-P-EPM': {
+    'xxx': {
         'DELETE':'RWD',
     },
 }
