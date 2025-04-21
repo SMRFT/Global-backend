1affa625cc31f01b39ff5e3ca9f47f402068e2b1
Ashokkumar T.A. <ashokkumar.ta@gmail.com>
2025-04-21 14:03:15 +0530

    Moved auth to external library

--- backend_diagnostics/views.py
+++ backend_diagnostics/views.py
@@ -14,7 +14,7 @@ from django.views.decorators.csrf import csrf_exempt
 from .models import Admin_groups
 from .serializers import  AdminSerializer
 from rest_framework import status
-from .auth.auth import HasRoleAndDataPermission
+from pyauth.auth import HasRoleAndDataPermission
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response  
 from rest_framework.decorators import api_view
